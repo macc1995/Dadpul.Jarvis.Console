a13e6c5edab259798b032d0390ebc8bdc94ccbaf
macc1995 <65418780+macc1995@users.noreply.github.com>
2026-07-23 00:13:14 +0200

    Add GitHub issue service contract

---
+++ src/Dadpul.Jarvis.Tools.GitHub/IGitHubIssueService.cs
@@ -0,0 +1,14 @@
+// Made by Dadpul
+
+namespace Dadpul.Jarvis.Tools.GitHub;
+
+internal interface IGitHubIssueService
+{
+   #region Public Methods and Operators
+
+   Task<GitHubIssueSearchResult> FindIssuesAsync(
+      GitHubIssueSearchRequest request,
+      CancellationToken cancellationToken);
+
+   #endregion
+}
